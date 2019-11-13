d24d3309a5097e5b56a7fb64a7bd1ee7d3341a37
HuihuiChyan <2745480383@qq.com>
2019-11-13 23:10:52 +0800

    Add files via upload

---
+++ text_classification/CNNClassifier/cnn.py
@@ -0,0 +1,94 @@
+﻿# -*- encoding: utf-8 -*-
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Dense, Input, Flatten
+from keras.layers import Reshape, Dropout, Concatenate
+from keras.layers import Conv2D, MaxPool2D, Embedding
+from keras.models import Model
+
+
+imdb = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
+
+word_index = imdb.get_word_index()
+
+# The first indices are reserved
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                       value=word_index["<PAD>"],
+                                                       padding='post',
+                                                       maxlen=256)
+
+
+# input shape is the vocabulary count used for the movie reviews (10,000 words)
+filter_sizes = [2,3,5]
+num_filters = 256
+drop = 0.5
+MAX_SEQUENCE_LENGTH=256
+EMBEDDING_DIM=100
+print("Creating Model...")
+inputs = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedding = Embedding(input_dim=len(word_index) + 1, output_dim=EMBEDDING_DIM,
+                      input_length=MAX_SEQUENCE_LENGTH)(inputs)
+reshape = Reshape((MAX_SEQUENCE_LENGTH,EMBEDDING_DIM,1))(embedding)
+
+conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], EMBEDDING_DIM), padding='valid', kernel_initializer='normal', activation='relu',activity_regularizer='l2')(reshape)
+conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], EMBEDDING_DIM), padding='valid', kernel_initializer='normal', activation='relu',activity_regularizer='l2')(reshape)
+conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], EMBEDDING_DIM), padding='valid', kernel_initializer='normal', activation='relu',activity_regularizer='l2')(reshape)
+
+maxpool_0 = MaxPool2D(pool_size=(MAX_SEQUENCE_LENGTH - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
+maxpool_1 = MaxPool2D(pool_size=(MAX_SEQUENCE_LENGTH - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
+maxpool_2 = MaxPool2D(pool_size=(MAX_SEQUENCE_LENGTH - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
+
+concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = Flatten()(concatenated_tensor)
+dropout = Dropout(drop)(flatten)
+preds = Dense(1, activation=tf.nn.sigmoid)(dropout)
+
+# this creates a model that includes inputs and outputs
+model = Model(inputs=inputs, outputs=preds)
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.summary()
+
+#创建验证集
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+#训练
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=64,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+print(results)
+
